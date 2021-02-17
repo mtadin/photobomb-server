a33f56fba1cd4fa69f6808bb6f8a70915419bd58
Marin Tadin <51207820+mtadin@users.noreply.github.com>
2021-02-17 16:24:45 +0100

    added comment

--- core/config/dev.js
+++ core/config/dev.js
@@ -2,5 +2,5 @@ module.exports = {
   port: 3000,
   mongo: {
     uri: 'mongodb+srv://test-user:photobomb1000@photobomb.tmrnp.mongodb.net/photobomb?retryWrites=true&w=majority'
-  }
+  } // replace this uri with your own mongodb connection link, this one is deleted
 }
